c6ab96bc8c8cd6b4caaa2f5e6e0c2c9d377bdfd7
superzhaozheng <80513871+superzhaozheng@users.noreply.github.com>
2021-08-16 21:40:16 +0800

    fix examples

--- examples/run/criteo_preprocess.sh
+++ examples/run/criteo_preprocess.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 wget https://labs.criteo.com/wp-content/uploads/2015/04/dac_sample.tar.gz -O tmp/dac_sample.tar.gz
-tar -xzf dac_sample.tar.gz -C tmp
+tar -xzf tmp/dac_sample.tar.gz -C tmp
 python3 examples/criteo_preprocess.py tmp/dac_sample.txt tmp/dac_sample.csv
 python3 criteo_lr_subclass.py --data tmp/dac_sample.csv --batch_size 256
